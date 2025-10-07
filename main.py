fc6d4aa6b5322e19042f993fd96af34a3e96fa4e
kennysc <kenny.stcyr@outlook.com>
2025-10-06 20:33:26 -0400

    some refactoring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-def isnumber(message):
+def is_number(message):
     while True:
         try:
             number = int(input(message))
@@ -12,28 +12,29 @@ def isnumber(message):
         except:
             print("Not a valid number, try again")
 
-def guesswithinlimits(higher_number, lower_number):
+def guess_within_limits(message, higher_number, lower_number):
     while True:
-        number = isnumber(f"Guess a number between {lower_number} and {higher_number}: ")
+        number = is_number(message)
         if lower_number <= number <= higher_number:
             return number
         else:
-            print(f"{number} is not between {lower_number} and {higher_number}")
+            print(
+                f"{number} is not between {lower_number} and {higher_number}"
+            )
 
 def main():
-    lower_number = isnumber("Enter the low number: ")
-    higher_number = isnumber("Enter the high number: ")
+    lower_number = is_number("Enter the low number: ")
+    higher_number = is_number("Enter the high number: ")
 
     while higher_number <= lower_number:
         print("The high number must be higher than the low number...")
-        higher_number = isnumber("Enter the high number: ")
+        higher_number = is_number("Enter the high number: ")
 
     random_number = randint(lower_number, higher_number)
     number_of_guesses = 10
     guess_count = 0
     too_low = False
     current_guess = None
-
 
     while guess_count < number_of_guesses:
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -43,8 +44,11 @@ def main():
             print(f"{current_guess} was too high")
 
         print(f"You have {number_of_guesses - guess_count} guesses left!")
-        #guess = isnumber(f"Guess un number between {lower_number} and {higher_number}: ")
-        guess = guesswithinlimits(higher_number, lower_number)
+        guess = guess_within_limits(
+            f"Guess a number between {lower_number} and {higher_number}: ",
+            higher_number,
+            lower_number
+        )
 
         if guess == random_number:
             print("Success! You found the random number!")
